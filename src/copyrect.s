d7bff6f583f7a74856922f775ead1ff64b5e8f25
Dieter Baron <dillo@nih.at>
2020-11-28 12:27:37 +0100

    Optimize copyriect.

--- src/copyrect.s
+++ src/copyrect.s
@@ -34,34 +34,33 @@
 ; copy rect from ptr1 to ptr2, x is width, y is height
 
 copyrect:
+	dex
 	stx ptr3
 	tya
 	tax
 
 line_loop:
-	ldy #0
+	ldy ptr3
 char_loop:
 	lda (ptr1),y
 	sta (ptr2),y
-	iny
-	cpy ptr3
-	bne char_loop
+	dey
+	bpl char_loop
 
 	; increment ptr1 by width, ptr2 by 40
-	clc
+	sec ; ptr3 is width - 1
 	lda ptr1
 	adc ptr3
 	sta ptr1
-	lda ptr1 + 1
-	adc #0
-	sta ptr1 + 1
-	lda ptr2
+	bcc :+
+	inc ptr1 + 1
+	clc
+:	lda ptr2
 	adc #40
 	sta ptr2
-	lda ptr2 + 1
-	adc #0
-	sta ptr2 + 1
-
+	bcc :+
+	inc ptr2 + 1
+:
 	dex
 	bne line_loop
 
